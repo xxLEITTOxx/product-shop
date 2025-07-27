49ebe31a25cc222b5c521f07724cf727df3171be
stanislavNemch <108931125+stanislavNemch@users.noreply.github.com>
2025-07-27 09:15:23 +0000

    Deploying to gh-pages from @ xxLEITTOxx/product-shop@07ab84abfaa9655468fa67b170370b38500bd17c 🚀

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-import"./assets/styles-JE8YjOlG.js";import{a as o}from"./assets/vendor-N5iQpiFS.js";function c(){const t=document.querySelector(".categories__btn");t&&t.classList.add("categories__btn--active")}const r="https://dummyjson.com/",a={CATEGORIES:"products/category-list",PRODUCTS:"products",PRODUCT_BY_ID:"products/",SEARCH:"products/search",PRODUCTS_BY_CATEGORY:"products/category/"};o.defaults.baseURL=r;async function n(){const{data:t}=await o(`${a.CATEGORIES}`);return t}const i={categoryList:document.querySelector(".categories")};function u(t){const e=t.map(s=>`<li class="categories__item">
-   <button class="categories__btn" type="button">${s}</button>
- </li>`).join("");i.categoryList.innerHTML=e}async function d(){try{const t=await n();u(["All",...t]),c()}catch(t){console.log(t)}}d();
+import"./assets/styles-JE8YjOlG.js";import{a}from"./assets/vendor-N5iQpiFS.js";function e(){const t=document.querySelector(".categories__btn");t&&t.classList.add("categories__btn--active")}const n="https://dummyjson.com/",r={CATEGORIES:"products/category-list",PRODUCTS:"products",PRODUCT_BY_ID:"products/",SEARCH:"products/search",PRODUCTS_BY_CATEGORY:"products/category/"},o={categoryList:document.querySelector(".categories"),productList:document.querySelector(".products")};a.defaults.baseURL=n;async function i(){const{data:t}=await a(`${r.CATEGORIES}`);return t}async function u(t){const s=(t-1)*12,c=`${r.PRODUCTS}?limit=12&skip=${s}`;return(await a(c)).data.products}function d(t){const s=t.map(c=>`<li class="categories__item">
+   <button class="categories__btn" type="button">${c}</button>
+ </li>`).join("");o.categoryList.innerHTML=s}function l(t){return`
+    <li class="products__item" data-id="${t.id}">
+      <img class="products__image" src="${t.thumbnail}" alt="${t.title}" />
+      <p class="products__title">${t.title}</p>
+      <p class="products__brand"><span class="products__brand--bold">Brand:</span> ${t.brand}</p>
+      <p class="products__category">Category: ${t.category}</p>
+      <p class="products__price">Price: $${t.price}</p>
+    </li>
+  `}function p(t){const s=t.map(l).join("");o.productList.innerHTML=s}let _=1;async function g(){try{const t=await i();d(["All",...t]),e()}catch(t){console.log(t)}}async function m(){try{const t=await u(_);p(t)}catch(t){console.log(t)}}g();m();
 //# sourceMappingURL=index.js.map
